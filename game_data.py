77ee64d52a6b33a4023e97eac1b7fd729dedff49
MD Irfanul Kabir Hira <106048128+irfanulkabirhira@users.noreply.github.com>
2025-01-01 22:39:54 +0600

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,47 @@
+# Dictionary
+
+data = [
+    {
+        'name': 'Instagram',
+        'Follower_count': 346,
+        'Description' : 'Social Media Platfrom',
+        'Country':'United State'
+    },
+    {
+        'name': 'Cristano Ronaldo',
+        'Follower_count': 215,
+        'Description' : 'Footballer',
+        'Country':'Portugal'
+    },
+    {
+        'name': 'Ariana granda',
+        'Follower_count': 183,
+        'Description' : 'Musician and actress',
+        'Country':'United State'
+    },
+    {
+        'name': 'Dwayan Johnson',
+        'Follower_count': 183,
+        'Description' : 'Actor',
+        'Country':'United State'
+    },
+    {
+        'name': 'Selena Gomez',
+        'Follower_count': 174,
+        'Description' : 'Musician and actress',
+        'Country':'United State'
+    },
+    {
+        'name': 'kylie Jenner',
+        'Follower_count': 172,
+        'Description' : 'Musician and actress',
+        'Country':'United State'
+    },
+    {
+        'name': 'Katy peri',
+        'Follower_count': 89,
+        'Description' : 'Musician and actress',
+        'Country':'United State'
+    },
+
+]
